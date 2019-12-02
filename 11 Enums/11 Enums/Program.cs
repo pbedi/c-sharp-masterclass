ccf0e4f866e5648c6ec359791402d310b9f31593
Philip <philip.bedi@berkeleygroup.co.uk>
2019-12-02 17:31:25 +0000

    Chapter 11 Enums nd Structs

---
+++ 11 Enums/11 Enums/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace _11_Enums
+{
+    enum Day { Mon, Tue, Wed, Thu, Fri, Sat, Sun};
+    enum Month { Jan = 1, Feb, Mar, Apr, May, Jun, Jul, Aug, Sep, Oct, Nov, Dec};
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Day fri = Day.Fri;
+            Day sun = Day.Sun;
+
+            Day a = Day.Fri;
+            Console.WriteLine(fri == a);
+
+            Console.WriteLine("Printing from Day Enum Monday and its position in the array");
+            Console.WriteLine(Day.Mon);
+            Console.WriteLine((int) Day.Mon);
+
+            Console.WriteLine("Printing from Month Enum of January after changing starting postion to 1 and its position in the array");
+            Console.WriteLine(Month.Jan);
+            Console.WriteLine((int)Month.Jan);
+            
+        }
+    }
+}
---
+++ 11 Structs/11 Structs/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace _11_Structs
+{
+    struct Game
+    {
+        public string name;
+        public string developer;
+        public double rating;
+        public string releaseDate;
+
+        public void Display()
+        {
+            Console.WriteLine("Game 1's name is {0}", name);
+            Console.WriteLine("Game 1's was developed by {0}", developer);
+            Console.WriteLine("Game 1's rating is {0}", rating);
+            Console.WriteLine("Game 1's was released on {0}", releaseDate);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Game game1;
+            game1.name = "Pokemon Go";
+            game1.developer = "Niantic";
+            game1.rating = 3.5;
+            game1.releaseDate = "01.07.2016";
+
+            game1.Display();
+        }
+    }
+}
