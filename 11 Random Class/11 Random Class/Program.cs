e0cb470a4b109f11eed080c5c1d28e52303231ee
Philip <philip.bedi@berkeleygroup.co.uk>
2019-12-04 18:44:38 +0000

    Math Class, Random Class and Regular Expression

---
+++ 11 Random Class/11 Random Class/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace _11_Random_Class
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Random Class!");
+            Random dice = new Random();
+            int numEyes;
+
+            for (int i=0; i < 10; i++)
+            {
+                numEyes = dice.Next(1, 7);
+                Console.WriteLine("{0}: {1}", i+1, numEyes);
+            }
+            Console.WriteLine("Ask a question?");
+            Console.ReadLine();
+            int answerNum = dice.Next(1, 7);
+            FortuneTeller(answerNum);
+        }
+
+        static void FortuneTeller(int num1)
+        {
+            string ans;
+            if (num1 > 1)
+            {
+                ans = "Yes";
+            } else if ( num1 < 1)
+            {
+                ans = "No";
+            } else
+            {
+                ans = "May Be";
+            }
+
+            Console.WriteLine(ans);
+        }
+    }
+}
---
+++ 11 Regular Expressions/11 Regular Expressions/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Net.Mail;
+using System.Text.RegularExpressions;
+
+namespace _11_Regular_Expressions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            string pattern = @"\d";
+            Regex regex = new Regex(pattern);
+
+            string text = "Hi there 77, my number is 12314";
+
+            MatchCollection matchColl = regex.Matches(text);
+            Console.WriteLine("{0} hits found:\n {1}", matchColl.Count, text);
+            foreach(Match hit in matchColl)
+            {
+                GroupCollection group = hit.Groups;
+                Console.WriteLine("{0} found at {1}", group[0].Value, group[0].Index);
+            }
+        }
+    }
+}
