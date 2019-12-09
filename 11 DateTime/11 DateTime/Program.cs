7d493c4a668ee5868e9b2f20436f15bae988810f
Philip <philip.bedi@berkeleygroup.co.uk>
2019-12-09 18:51:40 +0000

    11 DateTime and Nullables

---
+++ 11 DateTime/11 DateTime/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace _11_DateTime
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime dateTime = new DateTime(2019, 10, 18);
+            Console.WriteLine("My Friend's birthday is {0}", dateTime);
+            // write date time of today at screen
+            Console.WriteLine(DateTime.Today);
+            //write current time on screen
+            Console.WriteLine(DateTime.Now);
+            Console.WriteLine("Tomorrow's date is {0}", GetTomorrow());
+            Console.WriteLine("Today's day of the week is {0}", DateTime.Today.DayOfWeek);
+            Console.WriteLine("First day of the year in {0} was {1}", 1967, GetFirstDayOfYear(1999));
+            int days = DateTime.DaysInMonth(2000, 2);
+            Console.WriteLine("Days in the month of Feb were: {0}", days);
+            //current time
+            DateTime now = DateTime.Now;
+            Console.WriteLine("Minutes: {0}", now.Minute);
+
+            Console.WriteLine("{0} o'clock, {1} minutes and {2} seconds", now.Hour, now.Minute, now.Second);
+            Console.WriteLine("Write a date in this format: yyyy-mm-dd");
+            string input = Console.ReadLine();
+            DateTime dateTime1;
+            if(DateTime.TryParse(input, out dateTime1))
+            {
+                Console.WriteLine("You entered: {0}", dateTime1);
+                TimeSpan daysPassed = now.Subtract(dateTime1);
+                Console.WriteLine("Days passed since: {0}", daysPassed.Days);
+            } else
+            {
+                Console.WriteLine("Wrong input: {0}", input);
+            }
+        }
+
+        static DateTime GetTomorrow()
+        {
+            return DateTime.Today.AddDays(1);
+        }
+
+        static System.DayOfWeek GetFirstDayOfYear(int year)
+        {
+            DateTime dateTime = new DateTime(year, 1, 1);
+            return dateTime.DayOfWeek;
+        }
+    }
+}
---
+++ 11 Nulables/11 Nulables/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace _11_Nulables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int? num1 = null;
+            int? num2 = 1342;
+
+            double? num3 = new Double?();
+            double? num4 = 3.1425;
+
+            bool? boolval1 = new bool?();
+
+            Console.WriteLine("Our nullable numbers are: {0},{1},{2},{3}", num1, num2, num3,num4);
+            Console.WriteLine("Nullable boolean is: {0}", boolval1);
+
+            double? num5 = 13.1;
+            double? num6 = null;
+            double num7;
+
+            if(num5 == null)
+            {
+                num7 = 0.0;
+            } else
+            {
+                num7 = (double)num5;
+            }
+
+            Console.WriteLine("Value of num7 is {0}", num7);
+            // Shorter: The NULL coalesing operator ??
+            num7 = num6 ?? 8.53;
+            Console.WriteLine("Value of num7 is {0}", num7);
+            num7 = num5 ?? 8.53;
+            Console.WriteLine("Value of num7 is {0}", num7);
+        }
+    }
+}
