0f2fe9507aa4b30cfc9c7ace709a2f62110e6d2b
Philip <philip.bedi@berkeleygroup.co.uk>
2020-01-16 18:02:04 +0000

    List Box and event

---
+++ 13 WPF08 ListBox/13 WPF08 ListBox/MainWindow.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _13_WPF08_ListBox
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            List<Match> matches = new List<Match>();
+            matches.Add(new Match() { Team1 = "Bayern Munich", Team2 = "Real Madrid", Score1 = 3, Score2 = 2, Completion = 85 });
+            matches.Add(new Match() { Team1 = "Chelsea", Team2 = "Man United", Score1 = 5, Score2 = 2, Completion = 55 });
+            matches.Add(new Match() { Team1 = "PSG", Team2 = "Barca", Score1 = 1, Score2 = 1, Completion = 48 });
+            matches.Add(new Match() { Team1 = "BVB Dormund", Team2 = "AS Roma", Score1 = 3, Score2 = 2, Completion = 65 });
+            lbMatches.ItemsSource = matches;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if(lbMatches.SelectedItem != null)
+            {
+                MessageBox.Show("Selected Match: "
+                    + (lbMatches.SelectedItem as Match).Team1 + " "
+                    + (lbMatches.SelectedItem as Match).Score1 + ":"
+                    + (lbMatches.SelectedItem as Match).Score2 + " "
+                    + (lbMatches.SelectedItem as Match).Team2 + "\n and time left is: "
+                    + (90 - (lbMatches.SelectedItem as Match).Completion).ToString() + " minutes"
+                    );
+            } else
+            {
+                MessageBox.Show("Please select a game row!");
+            }
+        }
+    }
+
+    public class Match
+    {
+        public int Score1 { get; set; }
+        public int Score2 { get; set; }
+        public string Team1 { get; set; }
+        public string Team2 { get; set; }
+        public int Completion { get; set; }
+    }
+}
