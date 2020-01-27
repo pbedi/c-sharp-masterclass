ce0811f840c6c62ce12ab3cf58089146efc0412d
Philip <philip.bedi@berkeleygroup.co.uk>
2020-01-27 18:25:47 +0000

    WPF - Radio Button

---
+++ 13 WPF RadioButton/13 WPF RadioButton/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _13_WPF_RadioButton
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Yes_Checked(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Good choice, thanks");
+        }
+
+        private void No_Checked(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("You selected NO, Please say yes!");
+        }
+
+        private void Maybe_Checked(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("You selected, Maybe, Please say yes!");
+        }
+    }
+}
